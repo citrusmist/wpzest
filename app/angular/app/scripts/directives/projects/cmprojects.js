f1286b1ee286f7044ec0a15fba953da2652aeb78
Milos Soskic <artificermil@gmail.com>
2014-04-10 18:39:47 +0100

    Display project thumbnail on hover, glitchy

--- app/angular/app/scripts/directives/projects/cmprojects.js
+++ app/angular/app/scripts/directives/projects/cmprojects.js
@@ -10,16 +10,61 @@ angular.module('wpZest')
 			replace: true,
 			link: function postLink(scope, element, attrs) {
 				
-				var elPreview = angular.element(element[0].querySelectorAll('.projects-preview'));
-				var elList    = angular.element(element[0].querySelectorAll('.projects-list'));
-				var elTitles  = angular.element(element[0].querySelectorAll('.projects-title'));
+				var first         = true;
+				var elPreview     = angular.element(element[0].querySelectorAll('.projects-preview'));
+				var elPreviewWrap = angular.element(element[0].querySelectorAll('.projects-preview-wrap'));
+				var elList        = angular.element(element[0].querySelectorAll('.projects-list'));
+				var elTitles      = angular.element(element[0].querySelectorAll('.project-link'));
 
-				elList.bind('mouseenter', function() {
+				elList.on('mouseenter', function() {
 					element.addClass('projects--isActive');
+					if(first === true) {
+						elPreviewWrap.css({
+							'-webkit-transition-duration': '0s',
+					    '-moz-transition-duration': '0s',
+					    '-o-transition-duration': '0s',
+					    'transition-duration': '0s'
+						});
+						console.log(elPreviewWrap.attr('style'));
+					}
 				});
 
-				elList.bind('mouseleave', function() {
+				elList.on('mouseleave', function() {
 					element.removeClass('projects--isActive');
+					elPreview.removeClass('projects-preview--isActive');
+					first = true;
+				});
+
+				elTitles.on('mouseenter', function() {
+
+					var elTitle = angular.element(this);
+					var elThumb = angular.element(
+						elPreview[0].querySelectorAll(elTitle.data('thumb'))
+					);
+					var distanceY = elThumb[0].offsetTop;
+					console.log(elThumb[0].offsetTop);
+
+					elPreviewWrap.css({
+						'-webkit-transform': 'translateY(-' + distanceY + 'px)',
+						'-moz-transform': 'translateY(-' +  distanceY + 'px)',
+						'-ms-transform': 'translateY(-' + distanceY + 'px)',
+						'-o-transform': 'translateY(-' + distanceY + 'px)',
+						'transform': 'translateY(-' + distanceY + 'px)',
+					});
+
+					if(first === true) {
+						elPreviewWrap.css({
+							'-webkit-transition-duration': '',
+					    '-moz-transition-duration': '',
+					    '-o-transition-duration': '',
+					    'transition-duration': ''
+						});
+
+						first = false;
+					}
+
+					elPreview.addClass('projects-preview--isActive');
+
 				});
 			}
 		};
