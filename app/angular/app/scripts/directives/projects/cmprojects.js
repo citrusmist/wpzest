7af9edb25f5c523633bc8a4468267bdaef29389b
Milos Soskic <artificermil@gmail.com>
2014-04-13 00:37:55 +0100

    Use cmTranistion service for sniffing out transitionend and prefixing

--- app/angular/app/scripts/directives/projects/cmprojects.js
+++ app/angular/app/scripts/directives/projects/cmprojects.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('wpZest')
-	.directive('cmProjects', function () {
+	.directive('cmProjects', function(cmTransition) {
 
 		return {
 			template: '<div ng-transclude></div>',
@@ -18,24 +18,15 @@ angular.module('wpZest')
 
 				var slideIntoView = function(elThumb) {
 					var distanceY = elThumb[0].offsetTop;
-					elPreviewWrap.css({
-						'-webkit-transform': 'translateY(-' + distanceY + 'px)',
-						'-moz-transform': 'translateY(-' +  distanceY + 'px)',
-						'-ms-transform': 'translateY(-' + distanceY + 'px)',
-						'-o-transform': 'translateY(-' + distanceY + 'px)',
-						'transform': 'translateY(-' + distanceY + 'px)',
-					});
+					elPreviewWrap.css(
+						cmTransition.getPrefixed('transform', 'translateY(-' + distanceY + 'px)')
+					);
 				};
 
 				elList.on('mouseenter', function() {
 					element.addClass('projects--isActive');
 					if(first === true) {
-						elPreviewWrap.css({
-							'-webkit-transition-duration': '0s',
-					    '-moz-transition-duration': '0s',
-					    '-o-transition-duration': '0s',
-					    'transition-duration': '0s'
-						});
+						elPreviewWrap.css(cmTransition.getPrefixed('transition-duration', '0s'));
 					}
 				});
 
@@ -53,17 +44,12 @@ angular.module('wpZest')
 					);
 
 					if(first === true) {
-						elPreview.one('transitionend msTransitionEnd oTransitionEnd', function() {
+						elPreview.one(cmTransition.transitionEvent, function() {
 
 							slideIntoView(elThumb);
 
 							elPreviewWrap[0].offsetTop;
-							elPreviewWrap.css({
-								'-webkit-transition-duration': '',
-						    '-moz-transition-duration': '',
-						    '-o-transition-duration': '',
-						    'transition-duration': ''
-							});
+							elPreviewWrap.css(cmTransition.getPrefixed('transition-duration', '0s'));
 
 							elPreview.addClass('projects-preview--isActive');
 							first = false;
--- app/angular/app/scripts/services/cmTransition.js
+++ app/angular/app/scripts/services/cmTransition.js
@@ -2,27 +2,43 @@
 
 angular.module('wpZest')
   .factory('cmTransition', function () {
-    // Service logic
-    // ...
 
-    var transitionend = '';
+    var whichTransitionEvent = function() {
+
+      var t;
+      var el = document.createElement('fakeelement');
+      var transitions = {
+        'transition':'transitionend',
+        'MSTransition':'msTransitionEnd',
+        'MozTransition':'transitionend',
+        'WebkitTransition':'webkitTransitionEnd'
+      };
+   
+      for(t in transitions){
+        if( el.style[t] !== undefined ){
+          return transitions[t];
+        }
+      }
+    };
+
+    var transitionend = whichTransitionEvent();
 
     var getPrefixed = function(prop, val) {
 
-      var rules = {
-        prop: val
-      };
+      var rules = {};
 
       rules['-webkit-'+prop] = val;
       rules['-moz-'+prop]    = val;
       rules['-ms-'+prop]     = val;
       rules['-o-'+prop]      = val;
+      rules[prop]            = val;
 
       return rules;
     };
 
     // Public API here
     return {
-      getPrefixed: getPrefixed
+      getPrefixed: getPrefixed,
+      transitionEvent: transitionend
     };
   });
