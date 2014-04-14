2ecee003af349f6330a16b13073c8c74130cf129
Milos Soskic <artificermil@gmail.com>
2014-04-14 14:45:59 +0100

    Fix issue which caused the preview to not appear

--- app/angular/app/scripts/directives/projects/cmprojects.js
+++ app/angular/app/scripts/directives/projects/cmprojects.js
@@ -33,23 +33,26 @@ angular.module('wpZest')
 					);
 
 					if(timeout !== false) {
+						console.log('cancel timeout');
 						$timeout.cancel(timeout);
+						timeout = false;
 					}
 
 					if(first === true) {
-
+						console.log('showing preview');
 						element.addClass('projects--isActive');
 						elPreviewWrap.css(cmTransition.getPrefixed('transition-duration', '0s'));
 
 						elPreview.one(cmTransition.transitionEvent, function() {
+							//If the mouse hasn't moved from the title before the transition finished
+							if(element.hasClass('projects--isActive')) {
+								slideIntoView(elThumb);
+								elPreviewWrap[0].offsetTop;
+								elPreviewWrap.css(cmTransition.getPrefixed('transition-duration', ''));
 
-							slideIntoView(elThumb);
-
-							elPreviewWrap[0].offsetTop;
-							elPreviewWrap.css(cmTransition.getPrefixed('transition-duration', ''));
-
-							elPreview.addClass('projects-preview--isActive');
-							first = false;
+								elPreview.addClass('projects-preview--isActive');
+								first = false;
+							}
 						});
 					} else {
 						slideIntoView(elThumb);
@@ -57,12 +60,16 @@ angular.module('wpZest')
 				});
 
 				elTitles.on('mouseleave', function() {
-
+					console.log('leaving');
 					timeout = $timeout(function(){
+
+						first = true;
+						timeout = false;
+
 						element.removeClass('projects--isActive');
 						elPreview.removeClass('projects-preview--isActive');
-						first = true;
-					}, 400, false);
+						elPreviewWrap.css(cmTransition.getPrefixed('transition-duration', ''));
+					}, 400);
 				});
 			}
 		};
