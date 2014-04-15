b436c9954d9d05d6e32802f0733fbe4fd1479de3
Milos Soskic <artificermil@gmail.com>
2014-04-15 19:54:47 +0100

    Add cmProjectsPreview directive

--- app/angular/app/scripts/directives/projects/cmprojects.js
+++ app/angular/app/scripts/directives/projects/cmprojects.js
@@ -6,11 +6,16 @@ angular.module('wpZest')
 		return {
 			template: '<div ng-transclude></div>',
 			restrict: 'E',
+			controller: function($scope, $element, $attrs) {
+				this.first         = true;
+				this.slideIntoView = false;
+				this.showPreview   = false;
+				this.hidePreview   = false;
+			},
 			transclude: true,
 			replace: true,
-			link: function postLink(scope, element, attrs) {
+			link: function postLink(scope, element, attrs, controller) {
 				
-				var first         = true;
 				var elPreview     = angular.element(element[0].querySelectorAll('.projects-preview'));
 				var elPreviewWrap = angular.element(element[0].querySelectorAll('.projects-preview-wrap'));
 				var elList        = angular.element(element[0].querySelectorAll('.projects-list'));
@@ -33,40 +38,23 @@ angular.module('wpZest')
 						elPreview[0].querySelectorAll(elTitle.data('thumb'))
 					);
 
+					if(leaveTimeout !== false) {
+						console.log('cancel timeout');
+						$timeout.cancel(leaveTimeout);
+						leaveTimeout = false;
+					}
+
 					enterTimeout = $timeout(function () {
-
-						if(leaveTimeout !== false) {
-							console.log('cancel timeout');
-							$timeout.cancel(leaveTimeout);
-							leaveTimeout = false;
-						}
-
-						if(first === true) {
+						
+						if(controller.first === true) {
 							console.log('showing preview');
 							element.addClass('projects--isActive');
-							elPreviewWrap.css(cmTransition.getPrefixed('transition-duration', '0s'));
-
-							elPreviewWrap.on(cmTransition.transitionEvent, function(evt) {
-								evt.stopPropagation();
-							} );
-
-							elPreview.one(cmTransition.transitionEvent, function(evt) {
-								console.log(evt);
-								//If the mouse hasn't moved from the title before the transition finished
-								if(element.hasClass('projects--isActive')) {
-									slideIntoView(elThumb);
-									elPreviewWrap[0].offsetTop;
-									elPreviewWrap.css(cmTransition.getPrefixed('transition-duration', ''));
-
-									elPreview.addClass('projects-preview--isActive');
-									first = false;
-								}
-							});
+							
+							controller.showPreview(elThumb);
 						} else {
-							slideIntoView(elThumb);
+							controller.slideIntoView(elThumb);
 						}
 					}, 200);
-					
 				});
 
 				elTitles.on('mouseleave', function() {
@@ -79,14 +67,65 @@ angular.module('wpZest')
 					//TODO: check if the prjocects preview is active before setting hte timeoue
 					leaveTimeout = $timeout(function(){
 
-						first = true;
+						controller.first = true;
 						leaveTimeout = false;
 
 						element.removeClass('projects--isActive');
-						elPreview.removeClass('projects-preview--isActive');
-						elPreviewWrap.css(cmTransition.getPrefixed('transition-duration', ''));
+						
 					}, 400);
 				});
 			}
 		};
-	});
+	})
+	.directive('cmProjectsPreview', ['$timeout', 'cmTransition', function($timeout, cmTransition){
+		// Runs during compile
+		return {
+			// terminal: true,s
+			// scope: {}, // {} = isolate, true = child, false/undefined = no change
+			// controller: function($scope, $element, $attrs, $transclude) {},
+			require: '^cmProjects', // Array = multiple requires, ? = optional, ^ = check parent elements
+			restrict: 'E', // E = Element, A = Attribute, C = Class, M = Comment
+			template: '<div ng-transclude></div>',
+			transclude: true,
+			link: function(scope, element, attrs, controller) {
+
+				var elPreviewWrap = angular.element(element[0].querySelectorAll('.projects-preview-wrap'));
+
+				var slideIntoView = function(elThumb) {
+					var distanceY = elThumb[0].offsetTop;
+
+					elPreviewWrap.css(
+						cmTransition.getPrefixed('transform', 'translateY(-' + distanceY + 'px)')
+					);
+				};
+
+				var showPreview = function(elThumb) {
+					elPreviewWrap.css(cmTransition.getPrefixed('transition-duration', '0s'));
+
+					element.one(cmTransition.transitionEvent, function(evt) {
+						console.log(evt);
+						//If the mouse hasn't moved from the title before the transition finished
+						controller.slideIntoView(elThumb);
+						elPreviewWrap[0].offsetTop;
+						elPreviewWrap.css(cmTransition.getPrefixed('transition-duration', ''));
+
+						element.addClass('projects-preview--isActive');
+						controller.first = false;
+					});
+				};
+
+				var hidePreview = function() {
+					element.removeClass('projects-preview--isActive');
+					elPreviewWrap.css(cmTransition.getPrefixed('transition-duration', ''));
+				};
+
+				controller.slideIntoView = slideIntoView;
+				controller.showPreview = showPreview;
+				controller.hidePreview = hidePreview;
+
+				elPreviewWrap.on(cmTransition.transitionEvent, function(evt) {
+					evt.stopPropagation();
+				} );
+			}
+		};
+	}]);
