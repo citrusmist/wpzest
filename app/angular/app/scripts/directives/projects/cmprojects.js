95a3d98b0ac3e8ad88e2bece2a441396c963a07e
Milos Soskic <artificermil@gmail.com>
2014-05-22 17:11:25 +0100

    Fix bug causing the .project-preview to sometimes not appear

--- app/angular/app/scripts/directives/projects/cmprojects.js
+++ app/angular/app/scripts/directives/projects/cmprojects.js
@@ -1,100 +1,101 @@
 'use strict';
 
 angular.module('wpZest')
-	.directive('cmProjects', function($timeout, cmTransition) {
-
-		return {
-			template: '<div ng-transclude></div>',
-			restrict: 'E',
-			controller: function($scope, $element, $attrs) {
-				this.first         = true;
-				this.thumbRatio    = false;
-				this.slideIntoView = false;
-				this.showPreview   = false;
-				this.hidePreview   = false;
-				this.calcHeight    = false;
-				this.setupPreview  = false;
-			},
-			transclude: true,
-			replace: true,
-			link: function postLink(scope, element, attrs, controller) {
-				
-				var elPreview     = angular.element(element[0].querySelectorAll('.projects-preview'));
-				var elPreviewWrap = angular.element(element[0].querySelectorAll('.projects-preview-wrap'));
-				var elList        = angular.element(element[0].querySelectorAll('.projects-list'));
-				var elTitles      = angular.element(element[0].querySelectorAll('.project-link'));
-				var leaveTimeout  = false;
-				var enterTimeout  = false;
-
-				/*var slideIntoView = function(elThumb) {
-					var distanceY = elThumb[0].offsetTop;
-
-					elPreviewWrap.css(
-						cmTransition.getPrefixed('transform', 'translateY(-' + distanceY + 'px)')
-					);
-				};*/
-
-				elTitles.on('mouseenter', function() {
-
-					var elTitle = angular.element(this);
-					var elThumb = angular.element(
-						elPreview[0].querySelectorAll(elTitle.data('thumb'))
-					);
-
-					if(leaveTimeout !== false) {
-						console.log('cancel timeout');
-						$timeout.cancel(leaveTimeout);
-						leaveTimeout = false;
-					}
-
-					enterTimeout = $timeout(function () {
-						
-						if(controller.first === true) {
-							console.log('showing preview');
-							element.addClass('projects--isActive');
-							
-							controller.showPreview(elThumb);
-						} else {
-							controller.slideIntoView(elThumb);
-						}
-					}, 200);
-				});
-
-				elTitles.on('mouseleave', function() {
+	.directive('cmProjects', ['$timeout', 'cmUtil' , function($timeout, cmUtil) {
+	
+			return {
+				template: '<div ng-transclude></div>',
+				restrict: 'E',
+				controller: function($scope, $element, $attrs) {
+	
+					this.first           = true;
+					this.isPreviewActive = false;
+					this.thumbRatio      = false;
+					this.slideIntoView   = false;
+					this.showPreview     = false;
+					this.hidePreview     = false;
+					this.calcHeight      = false;
+					this.setupPreview    = false;
+	
+				},
+				transclude: true,
+				replace: true,
+				link: function postLink(scope, element, attrs, controller) {
 					
-					if(enterTimeout !== false) {
-						$timeout.cancel(enterTimeout);
-						enterTimeout = false;
-					}
-
-					//TODO: check if the projects preview is active before setting hte timeoue
-					leaveTimeout = $timeout(function(){
-
-						controller.first = true;
-						leaveTimeout = false;
-
-						element.removeClass('projects--isActive');
-						controller.hidePreview();
-					}, 400);
-				});
-
-				element.imagesLoaded()
-					.progress(function(instance,image){
-						
-						var dummyImg = new Image();
-
-						if(controller.thumbRatio !== false) {
-							return;
+					var elPreview     = angular.element(element[0].querySelectorAll('.projects-preview'));
+					// var elPreviewWrap = angular.element(element[0].querySelectorAll('.projects-preview-wrap'));
+					// var elList        = angular.element(element[0].querySelectorAll('.projects-list'));
+					var elTitles      = angular.element(element[0].querySelectorAll('.project-link'));
+					var leaveTimeout  = false;
+					var enterTimeout  = false;
+	
+					elTitles.on('mouseenter', function() {
+	
+						var elTitle = angular.element(this);
+						var elThumb = angular.element(
+							elPreview[0].querySelectorAll(elTitle.data('thumb'))
+						);
+	
+						if(leaveTimeout !== false) {
+							console.log('cancel leave timeout');
+							$timeout.cancel(leaveTimeout);
+							leaveTimeout = false;
 						}
+	
+						enterTimeout = $timeout(function () {
+							
+							enterTimeout = false;
 
-						dummyImg.src = image.img.src;
-						controller.thumbRatio = dummyImg.width / dummyImg.height;
-						console.log(controller.thumbRatio);
-						controller.setupPreview();
+							if(controller.isPreviewActive === false) {
+								console.log('showing preview');
+								controller.isPreviewActive = true;
+								element.addClass('projects--isActive');
+								
+								controller.showPreview(elThumb);
+							} else {
+								controller.slideIntoView(elThumb);
+							}
+						}, 200);
 					});
-			}
-		};
-	})
+	
+					elTitles.on('mouseleave', function() {
+						
+						if(enterTimeout !== false) {
+							console.log('cancel enter timeout');
+							$timeout.cancel(enterTimeout);
+							enterTimeout = false;
+						}
+	
+						//TODO: check if the projects preview is active before setting hte timeoue
+						leaveTimeout = $timeout(function(){
+	
+							controller.isPreviewActive = false;
+							leaveTimeout = false;
+	
+							element.removeClass('projects--isActive');
+							controller.hidePreview();
+						}, 400);
+					});
+	
+					element.imagesLoaded()
+						.progress(function(instance,image){
+							
+							var dummyImg = new Image();
+	
+							if(controller.thumbRatio !== false) {
+								return;
+							}
+	
+							dummyImg.src = image.img.src;
+							controller.thumbRatio = dummyImg.width / dummyImg.height;
+							console.log(controller.thumbRatio);
+							controller.setupPreview();
+						});
+
+					angular.element(window).on('resize', cmUtil.debounce(controller.setupPreview, 100));
+				}
+			};
+		}])
 	.directive('cmProjectsPreview', ['$timeout', 'cmTransition', function($timeout, cmTransition){
 		// Runs during compile
 		return {
@@ -124,14 +125,21 @@ angular.module('wpZest')
 					elPreviewWrap.css(cmTransition.getPrefixed('transition-duration', '0s'));
 
 					element.one(cmTransition.transitionEvent, function(evt) {
-						console.log(evt);
+
+						//in case this event handler got triggered by hiding transition
+						if(controller.isPreviewActive === false) {
+							return true;
+						}
+
+						// console.log(evt);
+
 						//If the mouse hasn't moved from the title before the transition finished
 						controller.slideIntoView(elThumb);
 						elPreviewWrap[0].offsetTop;
 						elPreviewWrap.css(cmTransition.getPrefixed('transition-duration', ''));
 
 						element.addClass('projects-preview--isActive');
-						controller.first = false;
+						// controller.first = false;
 					});
 				};
 
@@ -185,14 +193,11 @@ angular.module('wpZest')
 			    return ruleDefinition;
 				};
 
-
 				var setupPreview = function() {
 					console.log('setting up preview');
 					// element.css('height', controller.calcHeight());
 					var height = controller.calcHeight();
-					console.log(styleRules.style.height);
 					styleRules.style.height = height + 'px';
-					console.log( element);
 				};
 
 				controller.slideIntoView = slideIntoView;
@@ -202,6 +207,9 @@ angular.module('wpZest')
 				controller.setupPreview  = setupPreview;
 
 				elPreviewWrap.on(cmTransition.transitionEvent, function(evt) {
+					//Stop propagation so we dont get a false positive in showPreview()
+					//@TODO : consider an alternative approach
+
 					evt.stopPropagation();
 				});
 			}
