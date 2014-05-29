daa0a72fde34827eb41e3dd59749ebce2e372068
Milos Soskic <artificermil@gmail.com>
2014-05-29 19:53:02 +0100

    Use cmProjects service to retrieve project

--- app/angular/app/scripts/controllers/projects/project.js
+++ app/angular/app/scripts/controllers/projects/project.js
@@ -2,5 +2,8 @@
 
 angular.module('wpZestApp')
   .controller('ProjectsProjectCtrl', function ($scope, $routeParams, cmProjects) {
-		$scope.projectName = $routeParams.projectName;    
+		
+		cmProjects.findByName($routeParams.projectName).then(function(project) {
+			$scope.project = project;
+		});
   });
--- app/angular/app/scripts/services/projects/cmprojects.js
+++ app/angular/app/scripts/services/projects/cmprojects.js
@@ -17,7 +17,24 @@ angular.module('wpZestApp')
     };
 
     var findByName = function(name) {
+      
+      var projects = null;
+      var project  = $q.defer();
 
+      var lookupProjectByName = function() {
+        for (var i = projects.length - 1; i >= 0; i--) {
+          if(projects[i].postName === name) {
+            project.resolve(projects[i]);
+          }
+        }
+      };
+
+      all().then(function(data) {
+        projects = data;
+        lookupProjectByName();
+      });
+
+      return project.promise;
     };
 
     // Public API here
