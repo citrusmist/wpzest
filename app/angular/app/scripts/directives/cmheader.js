aa7ee1ce44438c43e60ecf6911a8826ac7520483
Milos Soskic <artificermil@gmail.com>
2014-05-29 14:52:38 +0100

    Assign seconary state to header when not on landing page

--- app/angular/app/scripts/directives/cmheader.js
+++ app/angular/app/scripts/directives/cmheader.js
@@ -1,13 +1,28 @@
 'use strict';
 
 angular.module('wpZestApp')
-  .directive('cmHeader', function () {
-    return {
-      templateUrl: 'views/cmHeader.html',
-      replace: true,
-      restrict: 'E',
-      link: function postLink(scope, element, attrs) {
-      }
-    };
-  });
+	.directive('cmHeader', function($route) {
+		return {
+			scope: {},
+			templateUrl: 'views/cmHeader.html',
+			replace: true,
+			restrict: 'E',
+			link: function postLink(scope, element, attrs) {
+
+				var determineState = function(currentRoute) {
+					if(currentRoute.$$route.controller === 'MainCtrl') {
+						element.removeClass('header--isSecondary');
+					} else {
+						element.addClass('header--isSecondary');
+					}
+				};
+
+				determineState($route.current);
+
+				scope.$on('$routeChangeSuccess', function(event, current) {
+					determineState(current);
+				});
+			}
+		};
+	});
 
