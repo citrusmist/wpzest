945ee3edef999c8f93b488cf13d80b50ae53d213
Milos Soskic <artificermil@gmail.com>
2014-05-29 14:48:01 +0100

    ProjectsProject controller and view

--- app/angular/app/scripts/controllers/projects/project.js
+++ app/angular/app/scripts/controllers/projects/project.js
@@ -1,10 +1,6 @@
 'use strict';
 
 angular.module('wpZestApp')
-  .controller('ProjectsProjectCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('ProjectsProjectCtrl', function ($scope, $routeParams, cmProjects) {
+		$scope.projectName = $routeParams.projectName;    
   });
