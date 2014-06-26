a4d7ad144f454beb7a3265ed5a00f89af54defd6
Milos Soskic <artificermil@gmail.com>
2014-06-26 19:15:12 +0100

    Close the header by clicking menu or outside of the header, emit headerStateChange when changing state

--- app/angular/app/scripts/directives/cmheader.js
+++ app/angular/app/scripts/directives/cmheader.js
@@ -1,26 +1,48 @@
 'use strict';
 
 angular.module('wpZestApp')
-	.directive('cmHeader', function($route) {
+	.directive('cmHeader', function($route, $rootElement) {
 		return {
 			scope: {},
 			templateUrl: 'views/cmHeader.html',
 			replace: true,
 			restrict: 'E',
 			controller: function($scope, $element, $attrs) {
-				this.state    = 'primary';
-				this.activate = function() {};
+				this.state        = 'primary';
+				this.activate     = function() {};
+				this.deactivate   = function() {};
+				this.isActive     = function() {};
+				this.toggleState  = function() {};
 			},
 			link: function postLink(scope, element, attrs, controller) {
 
 				var determineState = function(currentRoute) {
+
+					var initialState = controller.state;
+					var data = '';
+
 					if(currentRoute.$$route.controller === 'MainCtrl') {
+
 						element.removeClass('header--isSecondary');
 						element.removeClass('header--isActive');
+
 						controller.state = 'primary';
+						data             = '';
 					} else {
+
 						element.addClass('header--isSecondary');
+
 						controller.state = 'secondary';
+						data             = '.header--isSecondary';
+
+						if(controller.isActive()) {
+							controller.deactivate();
+						}
+					}
+
+					if(initialState !== controller.state) {
+
+						scope.$broadcast('headerStateChange', data);
 					}
 				};
 
@@ -28,12 +50,45 @@ angular.module('wpZestApp')
 					element.addClass('header--isActive');
 				};
 
+				var deactivate = function() {
+					element.removeClass('header--isActive');
+				};
+
+				var isActive = function() {
+					return element.hasClass('header--isActive');
+				};
+
+				var toggleState = function() {
+					if( controller.isActive() ) {
+						controller.deactivate();
+					} else {
+						controller.activate();
+					}
+				};
+
+				controller.activate    = activate;
+				controller.deactivate  = deactivate;
+				controller.isActive    = isActive;
+				controller.toggleState = toggleState;
+
 				scope.$on('$routeChangeSuccess', function(event, current) {
 					determineState(current);
 				});
 
+				$rootElement.on('click', function(evt) {
+
+					if(!controller.isActive()) {
+						return;
+					}
+
+					if(angular.element.contains(element[0], evt.target)) {
+						return;
+					}
+
+					controller.deactivate();
+				});
+
 				determineState($route.current);
-				controller.activate = activate;
 			}
 		};
 	})
@@ -47,8 +102,8 @@ angular.module('wpZestApp')
 			require: '^cmHeader',
 			link: function postLink(scope, element, attrs, controller) {
 
-				element.on('click', function(evt) {
-					controller.activate();
+				element.on('click', function() {
+					controller.toggleState();
 				});
 				
 			}
