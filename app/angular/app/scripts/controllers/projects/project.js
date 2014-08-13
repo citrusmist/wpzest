e3588fe471ac1100e958abc2cc4dcbd1e2c0eac7
Milos Soskic <artificermil@gmail.com>
2014-08-13 16:19:15 +0100

    Minor updates

--- app/angular/app/scripts/controllers/projects/project.js
+++ app/angular/app/scripts/controllers/projects/project.js
@@ -1,6 +1,7 @@
 'use strict';
 
 angular.module('wpZestApp')
-  .controller('ProjectsProjectCtrl', ['$scope', '$routeParams', '$sce', 'cmProjects', function ($scope, $routeParams, $sce, cmProjects) {
-		$scope.projectName = $routeParams.projectName;
-  }]);
+  .controller('ProjectsProjectCtrl', ['$scope', '$routeParams', '$sce', 'cmProjects', 
+  	function ($scope, $routeParams, $sce, cmProjects) {
+			$scope.projectName = $routeParams.projectName;
+	  }]);
